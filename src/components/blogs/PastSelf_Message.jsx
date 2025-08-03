295928fc6573100240b83c08c6617d5cff356244
csedge <ejlindayao@gmail.com>
2025-08-03 18:40:51 +0800

    A Message to My Past Self

--- src/components/blogs/PastSelf_Message.jsx
+++ src/components/blogs/PastSelf_Message.jsx
@@ -3,10 +3,63 @@ import React from "react";
 export const PastSelf_Message = () => {
   return (
     <div className="prose prose-invert prose-lg max-w-none">
-      <p className="text-xl">
-        July 24, 2025. If I could send a message to my past self, it would be:
+      <p className="text-xl font-bold">Dear Past Me,</p>
+      <p className="text-xl my-5">
+        I want you to know that you are doing great, even if it doesn't feel
+        like it sometimes. Life is a journey, and every step you take is part of
+        your growth and development as a person, so keep going!. If I could sit
+        with you for a moment, these are the five things I’d want you to hear:
       </p>
-      
+      <p className="text-3xl font-bold text-center">Be kind to yourself</p>
+      <p className="text-xl my-5">
+        I know you're very stressed when life gets tough or life doesn't go as
+        planned. I know you tend to blame yourself and carry more than you
+        should. Remember that it's okay to make mistakes. Mistakes are a part of
+        life, and they help you learn and grow. Be gentle with yourself, and
+        don't be afraid to ask for help when you need it.
+      </p>
+      <p className="text-3xl font-bold text-center">
+        Don't think what other people think
+      </p>
+      <p className="text-xl my-5">
+        Sometimes you rely too much on what others say about you. I know their
+        opinions can be helpful and give you new perspectives, but don’t let
+        them control how you see yourself. Remember, their words don’t define
+        your worth or your path. Be mindful of what you take in, trust your own
+        heart, and believe in your strength. You are doing better than you
+        realize, and your journey is yours alone to shape.
+      </p>
+      <p className="text-3xl font-bold text-center">Do it at your own pace</p>
+      <p className="text-xl my-5">
+        Don’t try to copy someone else’s process or their learning phase, doing
+        that can ruin your growth and trap you in a never ending loop. It’s okay
+        to take your time and move at your own pace. Trust your own rhythm,
+        listen to your heart, and remember that every small step forward is
+        progress worth celebrating.
+      </p>
+      <p className="text-3xl font-bold text-center">Take care of your body</p>
+      <p className="text-xl my-5">
+        You often turn to unhealthy foods when you’re stressed. But your body
+        deserves kindness and care. Try to eat healthy foods and exercise every
+        day, because the results are worth it. Taking care of your body will
+        give you more energy, clearer thoughts, and a stronger spirit to face
+        whatever comes your way.
+      </p>
+      <p className="text-3xl font-bold text-center">Take risk</p>
+      <p className="text-xl my-5">
+        Challenges may feel overwhelming at first, but they don’t last forever.
+        With time, things will get easier so don’t be afraid to face them head
+        on. You missed many opportunities, and don't wait for regret not
+        grabbing them when you had the chance. It’s okay to feel unsure or
+        awkward in the beginning, that’s how growth starts. Trust the process
+        and always have faith.
+      </p>
+
+      <p className="text-xl my-5">
+        With love,
+        <br />
+        <p className="text-xl font-bold">Your Future Self</p>
+      </p>
     </div>
   );
 };
